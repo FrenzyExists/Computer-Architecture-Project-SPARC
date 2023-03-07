c77789801c755e3055435a5d40eb3d8579ad3e44
Victor Hernandez <victor.hernandez25@upr.edu>
2023-03-07 09:48:25 -0400

    Register File v1.1

--- project/src/register-file.v
+++ project/src/register-file.v
@@ -1,5 +1,5 @@
 module register_file_3_port (
-  input [4:0] RA, RB, RC, // register select inputs for reading
+  input [4:0] RA, RB, RD, // register select inputs for reading
   input [4:0] RW, // register select input for writing
   input [31:0] PW, // data input for writing
   input LE, // latch enable input for writing
@@ -10,7 +10,7 @@ module register_file_3_port (
 reg [31:0] registers [0:31]; // array of 32 registers
 assign PA = (RA == 0) ? 0 : registers[RA]; // output for register RA
 assign PB = (RB == 0) ? 0 : registers[RB]; // output for register RB
-assign PD = (RC == 0) ? 0 : registers[RC]; // output for register RC
+assign PD = (RD == 0) ? 0 : registers[RD]; // output for register RC
 
 always @(posedge clk) begin
   if (LE) begin
@@ -22,7 +22,7 @@ endmodule
 
 module register_file_3_port_test;
 
-reg [4:0] RA, RB, RC, RW; // register select inputs for reading and writing
+reg [4:0] RA, RB, RD, RW; // register select inputs for reading and writing
 reg [31:0] PW; // data input for writing
 reg LE, clk; // latch enable input and clock input
 wire [31:0] PA, PB, PD; // output ports
@@ -30,7 +30,7 @@ wire [31:0] PA, PB, PD; // output ports
 register_file_3_port dut (
   .RA(RA),
   .RB(RB),
-  .RC(RC),
+  .RD(RD),
   .RW(RW),
   .PW(PW),
   .LE(LE),
@@ -44,7 +44,7 @@ initial begin
   // initialize inputs
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   RW = 0;
   PW = 20;
   LE = 1;
@@ -55,42 +55,42 @@ initial begin
   // read register 0 (output should be 0)
   RA = 0;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // read registers 0, 30, and 31
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // increment inputs
   PW = PW + 1;
   RW = RW + 1;
   RA = RA + 1;
   RB = RB + 1;
-  RC = RC + 1;
+  RD = RD + 1;
   
   // read registers again (output should be updated)
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // increment inputs again
   PW = PW + 1;
   RW = RW + 1;
   RA = RA + 1;
   RB = RB + 1;
-  RC = RC + 1;
+  RD = RD + 1;
   
   // read registers again (output should be updated)
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // keep incrementing inputs until RA reaches 31
   repeat (27) begin
@@ -98,9 +98,9 @@ initial begin
     RW = RW + 1;
     RA = RA + 1;
     RB = RB + 1;
-    RC = RC + 1;
+    RD = RD + 1;
     #4;
-    $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+    $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   end
 end
 
--- project/test/register-file test.v
+++ project/test/register-file test.v
@@ -1,6 +1,6 @@
 module register_file_3_port_test;
 
-reg [4:0] RA, RB, RC, RW; // register select inputs for reading and writing
+reg [4:0] RA, RB, RD, RW; // register select inputs for reading and writing
 reg [31:0] PW; // data input for writing
 reg LE, clk; // latch enable input and clock input
 wire [31:0] PA, PB, PD; // output ports
@@ -8,7 +8,7 @@ wire [31:0] PA, PB, PD; // output ports
 register_file_3_port dut (
   .RA(RA),
   .RB(RB),
-  .RC(RC),
+  .RD(RD),
   .RW(RW),
   .PW(PW),
   .LE(LE),
@@ -22,7 +22,7 @@ initial begin
   // initialize inputs
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   RW = 0;
   PW = 20;
   LE = 1;
@@ -33,42 +33,42 @@ initial begin
   // read register 0 (output should be 0)
   RA = 0;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // read registers 0, 30, and 31
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // increment inputs
   PW = PW + 1;
   RW = RW + 1;
   RA = RA + 1;
   RB = RB + 1;
-  RC = RC + 1;
+  RD = RD + 1;
   
   // read registers again (output should be updated)
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // increment inputs again
   PW = PW + 1;
   RW = RW + 1;
   RA = RA + 1;
   RB = RB + 1;
-  RC = RC + 1;
+  RD = RD + 1;
   
   // read registers again (output should be updated)
   RA = 0;
   RB = 31;
-  RC = 30;
+  RD = 30;
   #2;
-  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+  $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   
   // keep incrementing inputs until RA reaches 31
   repeat (27) begin
@@ -76,9 +76,9 @@ initial begin
     RW = RW + 1;
     RA = RA + 1;
     RB = RB + 1;
-    RC = RC + 1;
+    RD = RD + 1;
     #4;
-    $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RC, PW, PA, PB, PD);
+    $display("%d %d %d %d %d %d %d %d", RW, RA, RB, RD, PW, PA, PB, PD);
   end
 end
 
