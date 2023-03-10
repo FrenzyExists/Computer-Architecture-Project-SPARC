bec690cdd3c67e012723425bc59966295be9090f
Detective Pikachu <frenzyexists@gmail.com>
2023-03-10 10:19:24 -0400

    Updated Alu, alu tester kinda done, Operand Done, tester not so done

--- project/test/project-alu-tester.v
+++ project/test/project-alu-tester.v
@@ -1,12 +1,13 @@
 `timescale 1ns / 1ns
+`include "project/src/project-alu.v"
 
 module mini_alu_tb;
 
-  reg signed [31:0] a;
-  reg signed [31:0] b;
-  reg cin;
-  reg [3:0] opcode;
-  wire signed [31:0] y;
+  reg  [31:0] a;
+  reg  [31:0] b;
+  reg  cin;
+  reg  [3:0] opcode;
+  wire [31:0] y;
   wire [3:0] flags;
 
   mini_alu uut (
@@ -19,25 +20,62 @@ module mini_alu_tb;
   );
 
   initial begin
-  #3 $display("Test 1: postive number and negative number");
-    a = 32'b0000000000001100;
-    b = -32'b0000000000110011;
+  // #3 $display("Test 1: postive number and negative number");
+  // $display("Test 1: A: positive, B: negative, no carry \n");
+  #10
+    
+  //   a = 32'b0000000000001100;
+  //   b = -32'b0000000000110011;
+  //   cin = 1'b0;
+  //   opcode = 4'b0000;
+  //   repeat (15) #20 opcode = opcode + 4'b0001;
+
+  //   #100
+  //   $display("\n");
+
+    // #3 $display("Test 2: negative number and negative number");
+    // a = -32'b0000000000001100;
+    // b = -32'b0000000000110011;
+    // cin = 1'b0;
+    // opcode = 4'b0000;
+    // repeat (15) #20 opcode = opcode + 4'b0001;
+
+    // #100
+    // $display("\n");
+
+    // #3 $display("Test 2: negative number and negative number");
+    // a = 32'b01111111111111111111111111110011;
+    // b = 32'b0100000000001100;
+
+    // #100
+    // $display("\n");
+
+    // ---------------------------------------------------------
+
+    // a = 32'b00000000000000000000000000000001;
+    // b = 32'b10111111111111111111111111111111;
+
+    a = 32'b00000000000000000000000000000000;
+    b = 32'b00000000000000000000000000000001;
+
     cin = 1'b0;
     opcode = 4'b0000;
-    repeat (7) #10 opcode = opcode + 4'b0001;
+    repeat (15) #20 opcode = opcode + 4'b0001;
 
-    a = -32'b0000000000001100;
-    b = -32'b0000000000110011;
-    opcode = 4'b0000;
+    // #3 $display("Tesst 3: Numbers that trigger carry");
+    // a = 32'b1111111111111111;
+    // b = -32'b1111111111111111;
+    // cin = 1'b1;
+    // opcode = 4'b0000;
+    // repeat (7) #10 opcode = opcode + 4'b0001;
 
-repeat (7) #10 opcode = opcode + 4'b0001;
 
     // if (y !== 32'h00000000 || flags !== 4'b1000) $error("Test failed for ADD operation with flags");
     end
     initial begin
     
-    #4 $display("opcode |      A      |      B      |      Y      | flags");
-    #5 $monitor("%b   | %d | %d | %d | %b", opcode, a, b, y, flags);
+    #10 $display("opcode |      A      |      B      |      Y      | flags");
+    #15 $monitor("%b   | %d | %d | %d | %b | %b", opcode, a, b, y, flags, uut.carry);
     end
 
 
--- project/test/project-operand-tester.v
+++ project/test/project-operand-tester.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ns
+`include "project/src/project-operand.v"
 
 module operand_tb;
 
@@ -19,12 +20,15 @@ module operand_tb;
     // Test Main, taken from the example the professor gave us
     // TODO: Ask the professor for possible test case examples to include in this project
     R = 32'b11100000000000000000000000000011;
-    Imm = 21'b1000110001000100010011;
-
+    Imm = 21'b100110001000100010011;
+f
     #5 $display("Main Test for Operand. R=%b and Imm=%b. All following test will be outputting N for every change in IS", R, Imm);
     IS = 32'b0011111111111011111111110111111;
 
-    #5 $display("Test 1: IS=%b, N=%b", IS, N);
 
+    end
+
+    initial begin
+        #15 $monitor("%d | %d | %d | %d", IS, R, Imm, N);
     end
 endmodule
